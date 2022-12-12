2922bec79f1d8c23504e1474316ae2a8c1111b6e
tuananh168 <tuananh16895@gmail.com>
2022-12-12 23:10:25 +0700

    code page detail phim

--- cinecyberbug/src/Router/Router.jsx
+++ cinecyberbug/src/Router/Router.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
 import MainLayout from "../layout/MainLayout";
+import DetailFilm from "../page/DetailFilm/DetailFilm";
 import Home from "../page/home/Home";
 
 const Router = () => {
@@ -12,6 +13,10 @@ const Router = () => {
         {
           path: "/",
           element: <Home />,
+        },
+        {
+          path: "/detail/:id",
+          element: <DetailFilm />,
         },
       ],
     },
--- cinecyberbug/src/layout/MainLayout.jsx
+++ cinecyberbug/src/layout/MainLayout.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import DetailFilm from "../page/DetailFilm/DetailFilm";
 import Home from "../page/home/Home";
 import Footer from "./footer/Footer";
 import Header from "./header/Header";
@@ -11,6 +12,7 @@ const MainLayout = () => {
       <div className="main">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/detail/:id" element={<DetailFilm />} />
         </Routes>
       </div>
       <Footer />
